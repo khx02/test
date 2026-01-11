1728a59c8ad09d8fceacda7ee6ad4dc39bd39006
Kuah Hong Xuan <hkua0003@student.monash.edu>
2026-01-11 11:50:58 +0800

    chores: ran formatter

--- assignment2/main.py
+++ assignment2/main.py
@@ -2,6 +2,7 @@ import os
 import re
 from pathlib import Path
 from typing import Final, Match
+
 import regex_pattern
 
 """
@@ -11,25 +12,31 @@ Refactoring logic:
 3. More robust regex (Only allow 1 instance of the keyword) 
 """
 
-# Constants 
-BASE_DIR: Final[Path] = Path(os.path.join(os.environ["SourcePath"],"develop","global","src"))
-SCONSTRUCT_PATH: Final[Path] = Path(os.path.join(BASE_DIR, "SConstruct")) 
+# Constants
+BASE_DIR: Final[Path] = Path(
+    os.path.join(os.environ["SourcePath"], "develop", "global", "src")
+)
+SCONSTRUCT_PATH: Final[Path] = Path(os.path.join(BASE_DIR, "SConstruct"))
 VERSION_PATH: Final[Path] = Path(os.path.join(BASE_DIR, "VERSION"))
 
-BUILD_NUM: Final[str] = os.environ.get("BuildNum", "") 
+BUILD_NUM: Final[str] = os.environ.get("BuildNum", "")
 
-# Validation 
+
+# Validation
 def _validate_dir(dir: Path):
     if not dir.is_dir():
         raise NotADirectoryError(f"{dir} is not a directory")
+
 
 def _validate_file(file_path: Path):
     if not file_path.exists():
         raise FileNotFoundError(f"{file_path} can't be found")
 
+
 def _validate_is_int(num: str, var: str):
     if not num.strip().isdigit():
         raise ValueError(f"{var}: {num} is not integer.")
+
 
 def _chmod_to_executable(path: Path):
     try:
@@ -38,12 +45,14 @@ def _chmod_to_executable(path: Path):
         print("Warning: No Permission to chmod 755")
     except Exception as e:
         raise e
- 
+
+
 def validation(path: Path):
     _validate_dir(BASE_DIR)
     _validate_file(path)
     _validate_is_int(BUILD_NUM, "Environment Variable: BuildNum")
     _chmod_to_executable(path)
+
 
 def replace_in_file(path: Path, regex_pattern: re.Pattern[str], replacer_func):
     validation(path)
@@ -57,13 +66,10 @@ def replace_in_file(path: Path, regex_pattern: re.Pattern[str], replacer_func):
     match_obj = regex_pattern.search(file)
     new_block = replacer_func(match_obj)
 
-    new_content = (
-        file[:match_obj.start(1)] +
-        new_block +
-        file[match_obj.end(1):]
-    )
-        
+    new_content = file[: match_obj.start(1)] + new_block + file[match_obj.end(1) :]
+
     path.write_text(new_content, encoding="utf-8")
+
 
 # SCONSTRUCT file interesting lines
 # config.version = Version(
@@ -71,23 +77,25 @@ def replace_in_file(path: Path, regex_pattern: re.Pattern[str], replacer_func):
 # minor=0,
 # point=6,
 # patch=0
-#)
+# )
 def replace_Sconstruct(match: Match[str]) -> str:
     return re.sub(
         regex_pattern.POINT_PATTERN,
-        f'point={BUILD_NUM}',
+        f"point={BUILD_NUM}",
         match.group(0),
-        flags=re.IGNORECASE
+        flags=re.IGNORECASE,
     )
+
 
 # VERSION file interesting line
 # ADLMSDK_VERSION_POINT=6
 def replace_version(match: Match[str]) -> str:
     return re.sub(
         regex_pattern.VERSION_FILE_POINT_PATTERN,
-        rf'ADLMSDK_VERSION_POINT={BUILD_NUM}',
-        match.group(0)
+        rf"ADLMSDK_VERSION_POINT={BUILD_NUM}",
+        match.group(0),
     )
+
 
 def updateSconstruct():
     """
@@ -99,18 +107,25 @@ def updateSconstruct():
     patch=int
     )
     """
-    replace_in_file(SCONSTRUCT_PATH, regex_pattern.RE_PATTERN_SCONSTRUCT_VERSION, replace_Sconstruct)
+    replace_in_file(
+        SCONSTRUCT_PATH, regex_pattern.RE_PATTERN_SCONSTRUCT_VERSION, replace_Sconstruct
+    )
+
 
 def updateVersion():
     """
     Assume there'll always be the keyword ADLMSDK_VERSION_POINT=(int)
     """
-    replace_in_file(VERSION_PATH, regex_pattern.VERSION_FILE_POINT_PATTERN, replace_version)
+    replace_in_file(
+        VERSION_PATH, regex_pattern.VERSION_FILE_POINT_PATTERN, replace_version
+    )
+
 
 def main():
     updateSconstruct()
     print(f"Sconstruct point updated to {BUILD_NUM}")
     updateVersion()
     print(f"ADLMSDK version updated to {BUILD_NUM}")
+
 
 main()
